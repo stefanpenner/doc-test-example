c096e937e333cb8ea9913af3ea2160a4e0b15eb5
Stefan Penner <stefan.penner@gmail.com>
2019-02-13 11:03:56 -0800

    cleanup

--- test-setup.js
+++ test-setup.js
@@ -1,16 +1,21 @@
+const tmp = require('tmp');
 
+// tmp.setGracefulCleanup();
 const { getCode, parseFile } = require('doc-tester/lib/util');
 const { codeArray, importsArray } = parseFile('./README.md');
 const fs = require('fs-extra');
-const execa = require('execa');
 const path = require('path');
 
-// setup a test environment to mimic how a user will use our library.
-// We essentially put our library on the resolution path, so the tests can reference them via public API
+const TMP = `./tmp`;
 
-fs.removeSync('tmp');
-fs.mkdirSync('tmp');
-fs.mkdirSync('tmp/node_modules');
-fs.symlinkSync(__dirname, path.resolve('tmp/node_modules/doc-tester-example/'));
+// setup a test environment to mimic how a user will use our library. We
+// essentially put our library on the resolution path, so the tests can
+// reference them via public API
 
-fs.writeFileSync(__dirname + '/tmp/code.js', getCode(codeArray, importsArray, 'foo'));
+fs.removeSync(`${TMP}`);
+fs.mkdirSync(`${TMP}`);
+// setup a test environment to mimic
+fs.mkdirSync(`${TMP}/node_modules/`);
+fs.symlinkSync(__dirname, `${TMP}/node_modules/doc-tester-example`);
+
+fs.writeFileSync(`${TMP}/code.js`, getCode(codeArray, importsArray, 'foo'));
